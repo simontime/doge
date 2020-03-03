cd8e3d29cacb78ec004d822242835a8bdc542c69
Simon Aarons <40786398+simontime@users.noreply.github.com>
2020-03-04 09:19:47 +1100

    Add files via upload

---
+++ doge.c
@@ -0,0 +1,44 @@
+#include <Windows.h>
+
+static HHOOK hhk;
+
+LRESULT CALLBACK CBTProc(INT nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode == HCBT_ACTIVATE)
+	{
+		SetDlgItemText((HWND)wParam, IDOK, "WTF");
+		UnhookWindowsHookEx(hhk);
+	}
+	else
+	{
+		CallNextHookEx(hhk, nCode, wParam, lParam);
+	}
+	
+	return 0;
+}
+ 
+int CBTMessageBoxIndirect(const MSGBOXPARAMSA *lpmbp)
+{
+	hhk = SetWindowsHookEx(WH_CBT, &CBTProc, 0, GetCurrentThreadId());
+	return MessageBoxIndirect(lpmbp);
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	MSGBOXPARAMSA mbp;
+	
+	mbp.cbSize             = sizeof(MSGBOXPARAMSA);
+	mbp.hwndOwner          = NULL;
+	mbp.hInstance          = GetModuleHandle(NULL);
+	mbp.lpszText           = "DOGE IS HACKING YOUR COMPUTER";
+	mbp.lpszCaption        = "WTF";
+	mbp.dwStyle            = MB_OK | MB_USERICON;
+	mbp.lpszIcon           = MAKEINTRESOURCE(1);
+	mbp.dwContextHelpId    = NULL;
+	mbp.lpfnMsgBoxCallback = NULL;
+	mbp.dwLanguageId       = LANG_NEUTRAL;
+	
+	CBTMessageBoxIndirect(&mbp);
+	
+	return 0;
+}
