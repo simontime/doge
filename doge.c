8b5e839a11e9298904699bd6cbb7ef82ce564759
Simon Aarons <40786398+simontime@users.noreply.github.com>
2020-03-25 02:00:34 +1100

    Doge v2
    Now spawns two more windows every time you click "WTF"

--- doge.c
+++ doge.c
@@ -1,11 +1,28 @@
 #include <Windows.h>
 
+#define WINDOW_WIDTH  290
+#define WINDOW_HEIGHT 156
+
 static HHOOK hhk;
+static DWORD dwRand;
+
+DWORD Random()
+{
+	dwRand ^= (dwRand << 13);
+	dwRand ^= (dwRand >> 17);
+	dwRand ^= (dwRand <<  5);
+	
+	return dwRand;
+}
 
 LRESULT CALLBACK CBTProc(INT nCode, WPARAM wParam, LPARAM lParam)
 {
 	if (nCode == HCBT_ACTIVATE)
 	{
+		MoveWindow((HWND)wParam,
+			Random() % (GetSystemMetrics(SM_CXSCREEN) - WINDOW_WIDTH  + 1),
+			Random() % (GetSystemMetrics(SM_CYSCREEN) - WINDOW_HEIGHT + 1),
+			WINDOW_WIDTH, WINDOW_HEIGHT, FALSE);
 		SetDlgItemText((HWND)wParam, IDOK, "WTF");
 		UnhookWindowsHookEx(hhk);
 	}
@@ -23,7 +40,7 @@ int CBTMessageBoxIndirect(const MSGBOXPARAMSA *lpmbp)
 	return MessageBoxIndirect(lpmbp);
 }
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+DWORD WINAPI SpawnMessageBox(LPVOID lpParameter)
 {
 	MSGBOXPARAMSA mbp;
 	
@@ -34,11 +51,23 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	mbp.lpszCaption        = "WTF";
 	mbp.dwStyle            = MB_OK | MB_USERICON;
 	mbp.lpszIcon           = MAKEINTRESOURCE(1);
-	mbp.dwContextHelpId    = NULL;
+	mbp.dwContextHelpId    = (DWORD_PTR)NULL;
 	mbp.lpfnMsgBoxCallback = NULL;
 	mbp.dwLanguageId       = LANG_NEUTRAL;
 	
-	CBTMessageBoxIndirect(&mbp);
+	if (CBTMessageBoxIndirect(&mbp) == IDOK)
+	{
+		HANDLE hThread = CreateThread(NULL, 0, SpawnMessageBox, NULL, 0, NULL);
+		SpawnMessageBox(NULL);
+	}
+	
+	return 0;
+}
+
+INT WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nShowCmd)
+{
+	dwRand = GetTickCount();
+	SpawnMessageBox(NULL);
 	
 	return 0;
 }
